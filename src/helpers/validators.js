b733c302f6c770f62f7f65c6f415e3aa2367b912
Dima Kurbatov <dskurbatov13@gmail.com>
2019-01-01 17:37:15 -0800

    created helpers folder to store helper functions. Added validators file

---
+++ src/helpers/validators.js
@@ -0,0 +1,11 @@
+export const isValidEmail = (email) => {
+  return email.includes('@')
+}
+
+export const isValidPassword = (password) => {
+  return password.length > 1
+}
+
+export const isEmpty = (obj) => {
+  return Object.keys(obj).length === 0
+}
