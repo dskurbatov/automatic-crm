97119a7b14d95c2254b71dfeba72e96050757a30
Dima Kurbatov <dskurbatov13@gmail.com>
2019-01-01 21:21:56 -0800

    added router and state to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar'
+import ProgressBar from './components/ProgressBar'
+import Corporate from './components/Corporate'
+import Personal from './components/Personal'
+import SalesForce from './components/SalesForce'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
+  state = {
+    settings: {
+      isCorporateSet: false,
+      isPersonalSet: false,
+      isSalesForceSet: false
+    }
+  }
+
+  componentDidUpdate(){
+    console.log('app', this.state)
+  }
+  
+  onSubmit = (key) => (value) => {
+    this.setState(prevState => {
+      return {
+        ...prevState,
+        settings: {
+          ...prevState.settings,
+          [key]: value
+        }
+      }
+    })
+  }
+
   render() {
     return (
-      <React.Fragment className="text-center">
+      <React.Fragment>
         <Navbar />
         <div className="container">
           <h1 className="display-4">Welcome To Automatic-CRM</h1>
+          <ProgressBar settings={this.state.settings}/>
+          <Router>
+            <div>
+              <Route 
+                exact path='/' 
+                render={(props) => <Corporate {...props} onSubmit={this.onSubmit('isCorporateSet')} />}
+              />
+              <Route
+                path='/personal'
+                render={(props) => <Personal {...props} onSubmit={this.onSubmit('isPersonalSet')} />}
+                />
+              <Route 
+                path='/salesforce'
+                render={(props) => <SalesForce {...props} onSubmit={this.onSubmit('isSalesForceSet')} />}
+              />
+              <Route 
+                path='/analysis'
+                render={(props) => <div><h1 className="display-4">Congratulation You are Done!!!</h1></div>}
+              />
+              </div>
+          </Router>
         </div>
       </React.Fragment>
     );
