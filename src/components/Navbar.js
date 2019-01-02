e53ddf40de4f73a35b2042c929242ce51c99942d
Dima Kurbatov <dskurbatov13@gmail.com>
2019-01-02 01:23:08 -0800

    added bootstrap classes instead of hardcoded css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,19 @@ import React from 'react'
 const Navbar = (props) => {
   return(
     <nav className="navbar navbar-light">
-      <a className="navbar-brand" href="#">automaticCRM</a>
-      <ul className="nav justify-content-end">
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+      <a className="navbar-brand font-weight-bold mx-5" href="#">automaticCRM</a>
+      <ul className="nav">
+        <li className="nav-item mx-5">
+          <a className="nav-link text-uppercase text-dark" href="#">Features</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
+        <li className="nav-item mx-5">
+          <a className="nav-link text-uppercase text-dark" href="#">Pricing</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Faq</a>
+        <li className="nav-item mx-5">
+          <a className="nav-link text-uppercase text-dark" href="#">Faq</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About us</a>
+        <li className="nav-item mx-5">
+          <a className="nav-link text-uppercase text-dark" href="#">About us</a>
         </li>
       </ul>
     </nav>
