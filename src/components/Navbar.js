d01851bd4d9d97647df197788efc1bc0fc992ccb
Dima Kurbatov <dskurbatov13@gmail.com>
2019-01-02 01:45:24 -0800

    changed margin for progressBar and NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,18 @@ import React from 'react'
 const Navbar = (props) => {
   return(
     <nav className="navbar navbar-light">
-      <a className="navbar-brand font-weight-bold mx-5" href="#">automaticCRM</a>
+      <a className="navbar-brand font-weight-bold mx-3" href="#">automaticCRM</a>
       <ul className="nav">
-        <li className="nav-item mx-5">
+        <li className="nav-item mx-3">
           <a className="nav-link text-uppercase text-dark" href="#">Features</a>
         </li>
-        <li className="nav-item mx-5">
+        <li className="nav-item mx-3">
           <a className="nav-link text-uppercase text-dark" href="#">Pricing</a>
         </li>
-        <li className="nav-item mx-5">
+        <li className="nav-item mx-3">
           <a className="nav-link text-uppercase text-dark" href="#">Faq</a>
         </li>
-        <li className="nav-item mx-5">
+        <li className="nav-item mx-3">
           <a className="nav-link text-uppercase text-dark" href="#">About us</a>
         </li>
       </ul>
