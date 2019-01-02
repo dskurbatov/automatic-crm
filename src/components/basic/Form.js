68c1b6850c578f7779bb1c547a272deea04adf3a
Dima Kurbatov <dskurbatov13@gmail.com>
2019-01-01 16:25:10 -0800

    made new folder basic to store basic components and added info and form components to it

---
+++ src/components/basic/Form.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class Form extends React.Component {
+  state = this.props.initialState || {}
+  
+  onChange = (key) => (e) => {
+    this.setState({
+      [key]: e.target.value
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    this.props.onSubmit(this.state)
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.onSubmit} className="form">
+        {this.props.children(this.state, this.onChange)}
+      </form>
+    )
+  }
+}
+
+export default Form
---
+++ src/components/basic/Info.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Info = (props) => {
+  return (
+    <div className="info">
+      {props.text.map(text => <p>{text}</p>)}
+    </div>
+  )
+}
+
+export default Info
