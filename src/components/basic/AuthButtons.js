af5c1a8999d2c9061446a3d457ae7cda240e6a56
Dima Kurbatov <dskurbatov13@gmail.com>
2019-01-04 12:44:14 -0800

    added logic to handle production mode

--- src/components/basic/AuthButtons.js
+++ src/components/basic/AuthButtons.js
@@ -3,17 +3,26 @@ import { GoogleLogin } from 'react-google-login'
 import MicrosoftLogin from 'react-microsoft-login'
 import config from '../../../src/config'
 
+let googleId = null, microsoftId = null
+if(process.env.NODE_ENV === 'production'){
+  googleId = process.env.GOOGLE_CLIENTID
+  microsoftId = process.env.MICROSOFT_CLIENTID
+} else {
+  googleId = config.google.clientId
+  microsoftId = config.microsoft.clientId
+}
+
 const AuthButtons = (props) => {
   return (
     <div>
       <GoogleLogin
-        clientId={config.google.clientId}
+        clientId={googleId}
         buttonText="Sign in"
         onSuccess={props.onSignInG}
         onFailure={props.onError}
       />
       <MicrosoftLogin
-        clientId={config.microsoft.clientId}
+        clientId={microsoftId}
         buttonTheme={"light_short"}
         graphScopes={["user.read"]}
         withUserData={true}
