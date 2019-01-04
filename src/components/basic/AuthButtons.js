f80fc48739cc37d11287bece493ec1fea8d4e45b
Dima Kurbatov <dskurbatov13@gmail.com>
2019-01-04 13:38:17 -0800

    set variables for production

--- src/components/basic/AuthButtons.js
+++ src/components/basic/AuthButtons.js
@@ -1,28 +1,28 @@
 import React from 'react'
 import { GoogleLogin } from 'react-google-login'
 import MicrosoftLogin from 'react-microsoft-login'
-import config from '../../../src/config'
+//import config from '../../../src/config'
 
-let googleId = null, microsoftId = null
-if(process.env.NODE_ENV === 'production'){
-  googleId = process.env.GOOGLE_CLIENTID
-  microsoftId = process.env.MICROSOFT_CLIENTID
-} else {
-  googleId = config.google.clientId
-  microsoftId = config.microsoft.clientId
-}
+// let googleId = null, microsoftId = null
+// if(process.env.NODE_ENV === 'production'){
+//   googleId = process.env.GOOGLE_CLIENTID
+//   microsoftId = process.env.MICROSOFT_CLIENTID
+// } else {
+//   googleId = config.google.clientId
+//   microsoftId = config.microsoft.clientId
+// }
 
 const AuthButtons = (props) => {
   return (
     <div>
       <GoogleLogin
-        clientId={googleId}
+        clientId={process.env.GOOGLE_CLIENTID}
         buttonText="Sign in"
         onSuccess={props.onSignInG}
         onFailure={props.onError}
       />
       <MicrosoftLogin
-        clientId={microsoftId}
+        clientId={process.env.MICROSOFT_CLIENTID}
         buttonTheme={"light_short"}
         graphScopes={["user.read"]}
         withUserData={true}
